8208bb924447cb3b44a703197152fe093468d7b9
Hedde van der Heide <me@heddevanderheide.nl>
2016-07-27 12:28:08 +0200

    update poc

--- node-app/app.js
+++ node-app/app.js
@@ -5,10 +5,10 @@ var http = require('http');
 var port = process.env.npm_package_config_port || 8080;
 
 // Define body
-var name = process.env.npm_package_config_name || "app1";
+var name = process.env.npm_package_config_name || "undefined";
 var body = "<h1>" + name + "</h1>\n";
 
-// Configure our HTTP server to respond with Hello World to all requests.
+// Configure our HTTP server to respond with app name to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write(body);
